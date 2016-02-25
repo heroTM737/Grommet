4c8f3b7813aff2598610372597bb00c876e9aa90
heroTM737 <xuantien.nguyen93@gmail.com>
2016-02-25 21:35:13 +0700

    remove no public

--- ES6/src/js/widget/TreeGrid.js
+++
@@ -1,9 +0,0 @@
-import Tree from './Tree';
-// import ScrollBar from './ScrollBar';
-
-export default class TreeGrid extends Tree {
-  constructor() {
-    super();
-    console.log("it is in TreeGrid");
-  }
-};
--- POC/widget/messagetypeenum.js
+++
@@ -1,6 +0,0 @@
-var MessageTypeEnum={
-    ERROR:0,
-    WARNING:1,
-    CONFIRM:2,
-    INFO:3,
-};
